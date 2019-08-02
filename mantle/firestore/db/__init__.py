2f3ed917357c8405b9a3834ca0c91517acc2f468
Billcountry Mwaniki <me@billcountry.tech>
2019-08-02 11:30:56 +0300

    Removing db package from firestore

--- mantle/firestore/db/__init__.py
+++
@@ -1,7 +0,0 @@
-from mantle.firestore.db.model import Model
-from mantle.firestore.db.fields import ReferenceField, ListField, BooleanField, BytesField, DateTimeField, DictField,\
-    FloatingPointNumberField, IntegerField, StringField
-from mantle.firestore.db.errors import ReferenceFieldError, MalformedQueryError, InvalidValueError, SubCollectionError,\
-    InvalidPropertyError
-
-name = "db"
