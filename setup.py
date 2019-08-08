dfac37e05c269eb73e777917ac8581ff3eb686c0
Billcountry Mwaniki <me@billcountry.tech>
2019-08-08 14:32:36 +0300

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mantle-firestore",
-    version="0.0.1",
+    version="0.0.2",
     author="Billcountry Mwaniki",
     author_email="me@billcountry.tech",
     description="Implementation of models concept on top of Google cloud firestore",
