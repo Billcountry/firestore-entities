a3173045e9219a102d0e5659a9c19c243f3d8e15
Billcountry Mwaniki <me@billcountry.tech>
2019-08-08 12:06:52 +0300

    Cleaner smaller tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,6 @@
 import unittest
-from google.cloud.firestore_v1beta1 import SERVER_TIMESTAMP
-from mantle.firestore import Model
 from mantle import db
+from mantle.firestore import SERVER_TIMESTAMP
 import json
 from datetime import datetime, date
 from mockfirestore import MockFirestore
@@ -9,10 +8,9 @@ from mockfirestore import MockFirestore
 mock = MockFirestore()
 
 
-class TestDB(unittest.TestCase):
-    def test_fields(self):
-        # String Property
-        string_field = db.StringProperty(default="test", required=True, length=6)
+class TestProperties(unittest.TestCase):
+    def test_text_property(self):
+        string_field = db.TextProperty(default="test", required=True, length=6)
         self.assertEqual(string_field.validate("roast"), "roast")
         # The default value is set on an empty field
         self.assertEqual(string_field.validate(None), "test")
@@ -21,11 +19,11 @@ class TestDB(unittest.TestCase):
         #  String field should only accept strings
         self.assertRaises(db.InvalidValueError, string_field.validate, 123)
         # Create a field without a default value
-        string_field = db.StringProperty(required=True)
+        string_field = db.TextProperty(required=True)
         # This is a required field, None is not a valid value
         self.assertRaises(db.InvalidValueError, string_field.validate, None)
 
-        # Integer field
+    def test_integer_property(self):
         integer_field = db.IntegerProperty()
         self.assertEqual(integer_field.validate(1), 1)
         self.assertEqual(integer_field.validate(0), 0)
@@ -36,7 +34,7 @@ class TestDB(unittest.TestCase):
         # None required field defaults to None
         self.assertIsNone(integer_field.validate(None))
 
-        # Float field
+    def test_float_property(self):
         float_field = db.FloatingPointNumberProperty()
         # Should accept both float and integer values
         self.assertEqual(float_field.validate(0), 0)
@@ -45,13 +43,13 @@ class TestDB(unittest.TestCase):
         self.assertEqual(float_field.validate(3.142), 3.142)
         self.assertRaises(db.InvalidValueError, float_field.validate, "10234")
 
-        # List Property
+    def test_list_property(self):
         list_field = db.ListProperty(field_type=db.IntegerProperty())
         self.assertEqual(list_field.validate([1, 2, 3, 4]), [1, 2, 3, 4])
         # An invalid data type is not accepted as part of the child fields
         self.assertRaises(db.InvalidValueError, list_field.validate, [1, 2, ""])
 
-        # Dict Property
+    def test_dict_property(self):
         dict_field = db.DictProperty()
         valid_dict = dict(name="John doe", age=24)
         # A dict, a list, or a valid json string are valid values
@@ -60,7 +58,7 @@ class TestDB(unittest.TestCase):
         # A list is not a valid input of dict field
         self.assertRaises(db.InvalidValueError, dict_field.validate, [1, 2, 3])
 
-        # Date time Property
+    def test_datetime_property(self):
         dt_field = db.DateTimeProperty(auto_add_now=True)
         now = datetime.now()
         self.assertEqual(dt_field.validate(now), now)
@@ -76,7 +74,7 @@ class TestDB(unittest.TestCase):
         # This value is always updated to current time stamp on every update
         self.assertEqual(dt_field.validate(now), SERVER_TIMESTAMP)
 
-        # Boolean field
+    def test_boolean_property(self):
         bool_field = db.BooleanProperty(required=True)
         self.assertTrue(bool_field.validate(True))
         self.assertFalse(bool_field.validate(False))
@@ -85,14 +83,10 @@ class TestDB(unittest.TestCase):
         bool_field = db.BooleanProperty()
         self.assertIsNone(bool_field.validate(None))
 
-        # Bytes field
+    def test_bytes_property(self):
         some_bytes = bytes(b"bavnkvnkjwenegkv,erngvanavnwisnkversnvaern")
         bytes_field = db.BytesProperty(default=some_bytes)
         other_bytes = bytes(b"qwertytuyioutryewwertyrytuyterwqwewqrwetry")
         self.assertEqual(bytes_field.validate(None), some_bytes)
         self.assertEqual(bytes_field.validate(other_bytes), other_bytes)
         self.assertRaises(db.InvalidValueError, bytes_field.validate, "some_string")
-
-
-if __name__ == '__main__':
-    unittest.main()
