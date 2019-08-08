ee2c2362beda0f3be4f046cc94f69af677d9ac52
Billcountry Mwaniki <me@billcountry.tech>
2019-08-08 12:47:27 +0300

    Only add the needed packages

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mantle-studio/models",
-    packages=setuptools.find_packages(),
+    packages=['mantle', 'mantle.firestore'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
