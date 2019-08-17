688096f7c7d0d08762562ae6c4808c9f7704a62f
Billcountry Mwaniki <me@billcountry.tech>
2019-08-17 17:12:17 +0300

    Missing functionality

--- tests/run.py
+++ tests/run.py
@@ -2,12 +2,22 @@ import unittest
 import sys
 import os
 import google.cloud.firestore
+import uuid
 from mockfirestore import MockFirestore, CollectionReference, DocumentReference, DocumentSnapshot, Query
+
+
+def add(self, data):
+    _id = str(uuid.uuid4())
+    doc = self.document(_id)
+    doc.set(data)
+    return None, doc
+
 
 if __name__ == "__main__":
     # Add main dir to path
     sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
     # Don't run tests on actual firestore
+    setattr(CollectionReference, "add", add)
     google.cloud.firestore.Client = MockFirestore
     google.cloud.firestore.CollectionReference = CollectionReference
     google.cloud.firestore.DocumentSnapshot = DocumentSnapshot
